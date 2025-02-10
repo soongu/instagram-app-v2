b2eb0f7fb1e059dab1dc2cdfc01496b09ae307c3
soongu <naong526@naver.com>
2025-02-10 17:18:00 +0900

    #4. 회원가입 입력값 검증 - 정규표현식 및 빈값 에러메시지 처리

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -4,6 +4,8 @@ import styles from '../../pages/auth/SignupPage.module.scss';
 import {ValidationRules, checkPasswordStrength} from "../../utils/ValidationRules";
 
 const Signup = () => {
+
+  // 입력값들을 상태관리
   const [formData, setFormData] = useState({
     email: '',
     name: '',
@@ -11,11 +13,38 @@ const Signup = () => {
     password: '',
   });
 
+  // 입력 에러메시지를 상태관리
+  const [errors, setErrors] = useState({
+    email: '',
+    name: '',
+    username: '',
+    password: '',
+  });
+
+  // 입력값 검증을 수행하는 함수
+  const validateField = (fieldName, value) => {
+    let errorMessage = '';
+
+    if (!value) {
+      errorMessage = ValidationRules[fieldName].requiredMessage;
+    } else if (ValidationRules[fieldName].pattern && !ValidationRules[fieldName].pattern.test(value)) {
+      errorMessage = ValidationRules[fieldName].message;
+    }
+
+    return errorMessage;
+  };
+
   const handleChange = (e) => {
     const {name, value} = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
+    }));
+
+    const errorMessage = validateField(name, value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: errorMessage,
     }));
   };
 
@@ -35,6 +64,7 @@ const Signup = () => {
           value={formData.email}
           onChange={handleChange}
         />
+        {errors.email && <span className={styles.errorMessage}>{errors.email}</span>}
       </div>
 
       <div className={styles.formField}>
@@ -46,6 +76,7 @@ const Signup = () => {
           value={formData.name}
           onChange={handleChange}
         />
+        {errors.name && <span className={styles.errorMessage}>{errors.name}</span>}
       </div>
 
       <div className={styles.formField}>
@@ -57,6 +88,7 @@ const Signup = () => {
           value={formData.username}
           onChange={handleChange}
         />
+        {errors.username && <span className={styles.errorMessage}>{errors.username}</span>}
       </div>
 
       <div className={styles.formField}>
@@ -71,6 +103,8 @@ const Signup = () => {
           />
           <button type="button" className={styles.passwordToggle}>패스워드 표시</button>
         </div>
+
+        {errors.password && <span className={styles.errorMessage}>{errors.password}</span>}
       </div>
 
       <p className={styles.privacyText}>
