0bdfe5b445d7e3e6816870fcd76d4b0c2c044070
soongu <naong526@naver.com>
2026-03-19 09:43:04 +0900

    feat: redirect unauthenticated users to the root path when accessing protected routes.

--- src/routes/AuthRequired.jsx
+++ src/routes/AuthRequired.jsx
@@ -4,11 +4,16 @@
 import RootLayout from "../layouts/RootLayout.jsx";
 import AuthLayout from "../layouts/AuthLayout.jsx";
 import {useSelector} from "react-redux";
+import {Navigate, useLocation} from "react-router-dom";
 
 const AuthRequired = () => {
   const isAuthenticated = useSelector(state => state.auth.accessToken);
+  const location = useLocation();
 
   if (!isAuthenticated) {
+    if (location.pathname !== '/') {
+      return <Navigate to="/" replace />;
+    }
     return <AuthLayout isLoginPage={true}/>;
   }
 
